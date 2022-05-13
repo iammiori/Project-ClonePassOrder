507e86e8fd2f27873990a8816aa3147090bf2331
JeongDeokHo <ekdj98@gmail.com>
2022-05-13 11:47:28 +0900

    #7 feat 프로젝트 기본 설정

---
+++ Project-ClonePassOrder/MyPasser/MyPasserController.swift
@@ -0,0 +1,28 @@
+//
+//  MyPasserViewController.swift
+//  Project-ClonePassOrder
+//
+//  Created by 정덕호 on 2022/05/13.
+//
+
+import UIKit
+
+class MyPasserController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
