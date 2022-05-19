9bb9cedb7a4dcd22f12868ddb151ea6af022d87d
DeokHo98 <ekdj98@gmail.com>
2022-05-19 22:16:34 +0900

    #44 더보기뷰 레이아웃 수정

--- Project-ClonePassOrder/Home/ListController/MoreCollectionViewController.swift
+++ Project-ClonePassOrder/Home/ListController/MoreCollectionViewController.swift
@@ -7,27 +7,59 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class MoreCollectionViewController: UICollectionViewController {
+
+    //MARK: - 라이프사이클
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        setAtrribute()
     }
-
+    init() {
+        let layout = UICollectionViewCompositionalLayout { section, env in
+             let item = NSCollectionLayoutItem(
+                layoutSize: .init(widthDimension: .fractionalWidth(1),
+                                  heightDimension: .fractionalHeight(1))
+             )
+             item.contentInsets = NSDirectionalEdgeInsets(
+                top:0, leading: 0, bottom: 20, trailing: 20
+             )
+             let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: .init(widthDimension: .fractionalWidth(1),
+                                  heightDimension: .fractionalHeight(1/3)),
+                subitem: item, count: 2
+             )
+             group.contentInsets.leading = 20
+             let section = NSCollectionLayoutSection(group: group)
+            return section
+        }
+        super.init(collectionViewLayout: layout)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
-
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+    //MARK: - 메서드
+    
+    private func setAtrribute() {
+        self.collectionView!.register(SortCell.self, forCellWithReuseIdentifier: SortCell.identifier)
     }
 
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    //MARK: - 컬렉션뷰 데이터소스
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: SortCell.identifier,
+            for: indexPath
+        ) as! SortCell
+        cell.backgroundColor = .red
         return cell
     }
-
- 
 }
