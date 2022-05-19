22e13ea1f848ee20866a3726c7d16768678fbc2f
appleDevBooker <appledev.booker@gmail.com>
2022-05-19 19:33:25 +0900

    #43 feat: 인스턴스 프로퍼티 선언

--- Project-ClonePassOrder/qrCamera/qrCameraViewController.swift
+++ Project-ClonePassOrder/qrCamera/qrCameraViewController.swift
@@ -10,6 +10,12 @@ import SnapKit
 import AVFoundation
 
 class qrCameraViewController: UIViewController {
+    
+    // MARK: - Instance Properties
+    
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
