b66c3a4e4debec26a3543c097b7f1a8b64af5270
DeokHo98 <ekdj98@gmail.com>
2022-05-19 16:06:32 +0900

    #44 feat: 더보기 컬렉션뷰 컨트롤러 파일 생성

---
+++ Project-ClonePassOrder/Home/ListController/MoreCollectionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MoreCollectionViewController.swift
+//  Project-ClonePassOrder
+//
+//  Created by 정덕호 on 2022/05/19.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class MoreCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+    
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
+
+ 
+}
