7d7418dbd32e076d1ab9b0bd4366b1847cf73f41
DeokHo98 <ekdj98@gmail.com>
2022-05-19 15:32:17 +0900

    #28 프로퍼티 구현 완료

--- Project-ClonePassOrder/Home/ListView/ListCellFooter.swift
+++ Project-ClonePassOrder/Home/ListView/ListCellFooter.swift
@@ -9,4 +9,24 @@ import UIKit
 
 class ListCellFooter: UICollectionReusableView {
     
+    //MARK: - 식별자
+    
+    static let identifier = "ListCellFooter"
+    
+    //MARK: - 프로퍼티
+    
+    private let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "arrow.right")
+        iv.tintColor = .lightGray
+        iv.alpha = 0.6
+        return iv
+    }()
+    private let moreButton: UIButton = {
+        let bt = UIButton(type: .system)
+        bt.setTitle("더보기", for: .normal)
+        bt.setTitleColor(.black, for: .normal)
+        bt.titleLabel?.font = .systemFont(ofSize: 16)
+        return bt
+    }()
 }
