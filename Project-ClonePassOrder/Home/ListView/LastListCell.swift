519e256f5ac884fa8af47965eaa0c492a7b4bcba
DeokHo98 <ekdj98@gmail.com>
2022-05-20 12:53:16 +0900

    #41 LastCell 기본세팅

--- Project-ClonePassOrder/Home/ListView/LastListCell.swift
+++ Project-ClonePassOrder/Home/ListView/LastListCell.swift
@@ -8,5 +8,24 @@
 import UIKit
 
 class LastListCell: UICollectionViewCell {
+    //MARK: - 식별자
     
+    static let identifier = "LastListCell"
+    
+    //MARK: - 프로퍼티
+    
+    //MARK: - 라이프사이클
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - 메서드
+    
+    private func setLayout() {
+    }
 }
