86fc91ab25547782b55545fab847c61865d7e685
DeokHo98 <ekdj98@gmail.com>
2022-05-20 17:37:46 +0900

    #47 즐겨찾기 컬렉션뷰 기본세팅

--- Project-ClonePassOrder/Favorite/FavoriteViewController.swift
+++ Project-ClonePassOrder/Favorite/FavoriteViewController.swift
@@ -7,23 +7,35 @@
 
 import UIKit
 
-class FavoriteViewController: UIViewController {
+class FavoriteViewController: UICollectionViewController {
 
+    //MARK: - 라이프사이클
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
-        // Do any additional setup after loading the view.
+        setAtrribute()
+        naviSetAttribute()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - 메서드
+    
+    private func setAtrribute() {
     }
-    */
-
+    private func naviSetAttribute() {
+    }
+    
+    //MARK: - 컬렉션뷰 데이터소스
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+    }
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+    }
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        
+    }
 }
