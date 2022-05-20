5e17f9f40d65400d13c0c388e3a24a09ac5d41b8
DeokHo98 <ekdj98@gmail.com>
2022-05-20 12:02:50 +0900

    #46 셀 기본 설정

--- Project-ClonePassOrder/Home/Search/SearchCollectionViewController.swift
+++ Project-ClonePassOrder/Home/Search/SearchCollectionViewController.swift
@@ -11,15 +11,30 @@ class SearchCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setAtrribute()
+    }
+    
+    //MARK: - 메서드
+    private func setAtrribute() {
+        collectionView.register(SortCell.self, forCellWithReuseIdentifier: SortCell.identifier)
     }
 
     //MARK: - 컬렉션뷰 데이터소스
 
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
         return 0
     }
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: SortCell.identifier,
+            for: indexPath
+        )
         return cell
     }
 
