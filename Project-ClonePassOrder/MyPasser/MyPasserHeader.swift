b7a3ebcd81ef371691351a175824553d4a4d05c4
DeokHo98 <ekdj98@gmail.com>
2022-05-21 13:26:13 +0900

    #48 feat: 헤더 프로퍼티 추가

--- Project-ClonePassOrder/MyPasser/MyPasserHeader.swift
+++ Project-ClonePassOrder/MyPasser/MyPasserHeader.swift
@@ -10,11 +10,28 @@ import UIKit
 class MyPasserHeader: UITableViewHeaderFooterView {
     
     //MARK: - 식별자
+    
     static let identifier: String = "MyPasserHeader"
     
     //MARK: - 프로퍼티
     
+    private let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .lightGray
+        iv.clipsToBounds = true
+        iv.layer.cornerRadius = 1
+        return iv
+    }()
+    private let label: UILabel = {
+        let lb = UILabel()
+        lb.text = "정덕호"
+        lb.textColor = .black
+        lb.font = .boldSystemFont(ofSize: 20)
+        return lb
+    }()
+    
     //MARK: - 라이프사이클
+    
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
         setLayout()
@@ -23,7 +40,9 @@ class MyPasserHeader: UITableViewHeaderFooterView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     //MARK: - 메서드
+    
     private func setLayout() {
         
     }
