033c3e3feb060d43643b835efc4f5cb13d3f16b8
Mi Yeon Lee <immioriii@gmail.com>
2022-05-21 17:14:35 +0900

    feat: 스크린 사이즈 constant 추가

---
+++ Project-ClonePassOrder/Utils/Constant/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  Project-ClonePassOrder
+//
+//  Created by miori Lee on 2022/05/18.
+//
+
+import UIKit
+
+//MARK: 디바이스의 스크린 크기
+
+struct ScreenConstant {
+    static let deviceWidth = UIScreen.main.bounds.width
+    static let deviceHeight = UIScreen.main.bounds.height
+}
