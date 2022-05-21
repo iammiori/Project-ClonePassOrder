5001f0c5695143c7f39534bfcc3db59643dbf08a
DeokHo98 <ekdj98@gmail.com>
2022-05-21 12:18:43 +0900

    #48 테이블뷰 셀 생성

---
+++ Project-ClonePassOrder/MyPasser/MyPasserCell.swift
@@ -0,0 +1,29 @@
+//
+//  MyPasserCell.swift
+//  Project-ClonePassOrder
+//
+//  Created by 정덕호 on 2022/05/21.
+//
+
+import UIKit
+
+class MyPasserCell: UITableViewCell {
+    
+    //MARK: - 프로퍼티
+    
+    //MARK: - 라이프사이클
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    //MARK: - 메서드
+    private func setLayout() {
+        
+    }
+
+}
