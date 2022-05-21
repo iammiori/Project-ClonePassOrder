a3cac0201a7122add8d61098017a06b3cde3f1af
DeokHo98 <ekdj98@gmail.com>
2022-05-21 13:22:47 +0900

    #48 feat: 헤더 기본설정

--- Project-ClonePassOrder/MyPasser/MyPasserHeader.swift
+++ Project-ClonePassOrder/MyPasser/MyPasserHeader.swift
@@ -9,5 +9,20 @@ import UIKit
 
 class MyPasserHeader: UITableViewHeaderFooterView {
     
-
+    //MARK: - 식별자
+    static let identifier: String = "MyPasserHeader"
+    
+    //MARK: - 프로퍼티
+    
+    //MARK: - 라이프사이클
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    //MARK: - 메서드
+    private func setLayout() {
+        
+    }
 }
