025815906c86345521837d241a17a39227f1c342
DeokHo98 <ekdj98@gmail.com>
2022-05-21 12:44:26 +0900

    #48 feat: 테이블뷰 기본속성 세팅 및 셀 추가

--- Project-ClonePassOrder/MyPasser/MyPasserTableViewController.swift
+++ Project-ClonePassOrder/MyPasser/MyPasserTableViewController.swift
@@ -13,21 +13,35 @@ class MyPasserTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setAtrribute()
     }
     
     //MARK: - 메서드
     private func setAtrribute() {
-        
+        tableView.register(
+            MyPasserCell.self,
+            forCellReuseIdentifier: MyPasserCell.identifier
+        )
+        tableView.rowHeight = 50
+        tableView.separatorStyle = .none
     }
     
     //MARK: - 테이블뷰 데이터소스
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 5
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: MyPasserCell.identifier,
+            for: indexPath
+        ) as! MyPasserCell
+        cell.label.text = "\u{1F44D}\u{1F3FB}   내 스토리"
+        return cell
     }
 
 }
