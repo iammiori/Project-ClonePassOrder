6d18f3fc2539bd21a871f4521289d74c3bb1647c
DeokHo98 <ekdj98@gmail.com>
2022-05-21 12:15:22 +0900

    #48 tableviewController 생성

---
+++ Project-ClonePassOrder/MyPasser/MyPasserTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MyPasserViewController.swift
+//  Project-ClonePassOrder
+//
+//  Created by 정덕호 on 2022/05/13.
+//
+
+import UIKit
+
+class MyPasserTableViewController: UITableViewController {
+
+    //MARK: - 라이프사이클
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MARK: - 테이블뷰 데이터소스
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
