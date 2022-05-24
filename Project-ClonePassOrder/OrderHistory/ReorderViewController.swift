17ef9ddd196d459699d0c89ade013c5c85aded92
appleDevBooker <appledev.booker@gmail.com>
2022-05-25 02:04:57 +0900

    #70 refactor: 오타 수정

--- Project-ClonePassOrder/OrderHistory/ReorderViewController.swift
+++ Project-ClonePassOrder/OrderHistory/ReorderViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class ReOrderViewController: UIViewController {
+class ReorderViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
