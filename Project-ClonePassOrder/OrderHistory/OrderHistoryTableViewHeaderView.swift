e5c9320943d6d38c4486dce8fa1baddc3ff497e7
appleDevBooker <appledev.booker@gmail.com>
2022-05-24 22:00:14 +0900

    #65 style: 불필요 공백 제거

--- Project-ClonePassOrder/OrderHistory/OrderHistoryTableViewHeaderView.swift
+++ Project-ClonePassOrder/OrderHistory/OrderHistoryTableViewHeaderView.swift
@@ -34,7 +34,6 @@ class OrderHistoryTableViewHeaderView: UITableViewHeaderFooterView {
         super.init(reuseIdentifier: reuseIdentifier)
         setLayout()
     }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
