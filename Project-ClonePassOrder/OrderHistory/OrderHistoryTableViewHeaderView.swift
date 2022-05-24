8f2ca7b86eeb324aa10a3a9353280fcd794a5885
appleDevBooker <appledev.booker@gmail.com>
2022-05-24 12:42:50 +0900

    #65 OrderHistoryTableViewHeaderView의 UI Properties 선언

--- Project-ClonePassOrder/OrderHistory/OrderHistoryTableViewHeaderView.swift
+++ Project-ClonePassOrder/OrderHistory/OrderHistoryTableViewHeaderView.swift
@@ -9,4 +9,22 @@ import UIKit
 import SnapKit
 
 class OrderHistoryTableViewHeaderView: UITableViewHeaderFooterView {
+    
+    // MARK: - UI Properties
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "주문내역"
+        label.font = .boldSystemFont(ofSize: 50)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let orderDateSelectButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("2022년 05월 >", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.contentHorizontalAlignment = .left
+        return button
+    }()
 }
