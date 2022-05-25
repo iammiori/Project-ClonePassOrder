d7a249f78976f5f064247788b7451cc362ba7f68
appleDevBooker <appledev.booker@gmail.com>
2022-05-25 15:13:40 +0900

    #65 refactor: 상속 제한 및 접근제어 설정

--- Project-ClonePassOrder/OrderHistory/OrderHistoryTableViewHeaderView.swift
+++ Project-ClonePassOrder/OrderHistory/OrderHistoryTableViewHeaderView.swift
@@ -8,18 +8,18 @@
 import UIKit
 import SnapKit
 
-class OrderHistoryTableViewHeaderView: UITableViewHeaderFooterView {
+final class OrderHistoryTableViewHeaderView: UITableViewHeaderFooterView {
     
     // MARK: - UI Properties
     
-    let titleLabel: UILabel = {
+    private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "주문내역"
         label.font = .boldSystemFont(ofSize: 50)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    let orderDateSelectButton: UIButton = {
+    private let orderDateSelectButton: UIButton = {
         let button = UIButton()
         button.setTitle("2022년 05월 >", for: .normal)
         button.setTitleColor(.black, for: .normal)
