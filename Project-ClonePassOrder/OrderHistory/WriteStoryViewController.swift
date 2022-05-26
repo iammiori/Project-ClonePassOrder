6476e86a282939347cf2ea090bdcf11935a51f95
appleDevBooker <appledev.booker@gmail.com>
2022-05-26 18:05:33 +0900

    #71 feat: 덱스트뷰 작성을 시작하면 작성하기 버튼 키보드 위로 위치시키기 구현

--- Project-ClonePassOrder/OrderHistory/WriteStoryViewController.swift
+++ Project-ClonePassOrder/OrderHistory/WriteStoryViewController.swift
@@ -95,6 +95,19 @@ final class WriteStoryViewController: UIViewController {
         super.viewDidLoad()
         setNavigation()
         setLayout()
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
     }
 
     // MARK: - setLayout
@@ -160,5 +173,11 @@ final class WriteStoryViewController: UIViewController {
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
+    }
+    @objc func keyboardWillShow(_ sender: Notification) {
+        self.view.frame.origin.y = -320
+    }
+    @objc func keyboardWillHide(_ sender: Notification) {
+        self.view.frame.origin.y = 0
     }
 }
