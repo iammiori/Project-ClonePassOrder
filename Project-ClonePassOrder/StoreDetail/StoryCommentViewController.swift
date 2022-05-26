4892a5c4b06e426d6dc1000677ec73f6222636a4
appleDevBooker <appledev.booker@gmail.com>
2022-05-26 16:54:41 +0900

    #10 feat: 텍스트 필드 플레이스홀더의 패딩설정을 위한 함수 구현(UITextField 확장)

--- Project-ClonePassOrder/StoreDetail/StoryCommentViewController.swift
+++ Project-ClonePassOrder/StoreDetail/StoryCommentViewController.swift
@@ -9,4 +9,13 @@ import UIKit
 import SnapKit
 
 class StoryCommentViewController: UIViewController {
+
+// MARK: - extension UITextField
+
+extension UITextField {
+    func addLeftPadding() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
 }
