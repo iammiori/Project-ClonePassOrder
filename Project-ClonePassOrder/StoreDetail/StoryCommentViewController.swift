d752b03b271bb2ce5b0faecc5198dd91b3eb3717
appleDevBooker <appledev.booker@gmail.com>
2022-05-26 16:53:03 +0900

    #10 StoryCommentViewController 파일 생성 및 선언
    - 스토리 댓글 화면

---
+++ Project-ClonePassOrder/StoreDetail/StoryCommentViewController.swift
@@ -0,0 +1,12 @@
+//
+//  StoryCommentViewController.swift
+//  Project-ClonePassOrder
+//
+//  Created by Eunsoo KIM on 2022/05/26.
+//
+
+import UIKit
+import SnapKit
+
+class StoryCommentViewController: UIViewController {
+}
