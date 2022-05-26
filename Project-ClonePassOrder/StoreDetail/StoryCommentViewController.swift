2ab6bdadf6f1852fdfc7285246227e6fa9ab09c0
appleDevBooker <appledev.booker@gmail.com>
2022-05-26 16:56:03 +0900

    #10 feat: StoryCommentViewController의 UI Properties 선언

--- Project-ClonePassOrder/StoreDetail/StoryCommentViewController.swift
+++ Project-ClonePassOrder/StoreDetail/StoryCommentViewController.swift
@@ -10,6 +10,34 @@ import SnapKit
 
 class StoryCommentViewController: UIViewController {
 
+    // MARK: - UI Properties
+
+    let commentTableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    let floatingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    let commentTextField: UITextField = {
+        let textField = UITextField()
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius = 20
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.systemGray.cgColor
+        textField.placeholder = "댓글을 입력해 주세요"
+        textField.addLeftPadding()
+        return textField
+    }()
+    let commentWriteCompletButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("확인", for: .normal)
+        button.setTitleColor(.gray, for: .normal)
+        button.isEnabled = false
+        return button
+    }()
 // MARK: - extension UITextField
 
 extension UITextField {
