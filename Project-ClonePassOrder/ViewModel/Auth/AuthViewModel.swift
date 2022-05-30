8236877ca6e0f2f8984a77af4f1c23c31b56c906
DeokHo98 <ekdj98@gmail.com>
2022-05-30 15:25:10 +0900

    #91 reactor: viewModel 리팩토링

--- Project-ClonePassOrder/ViewModel/Auth/AuthViewModel.swift
+++ Project-ClonePassOrder/ViewModel/Auth/AuthViewModel.swift
@@ -7,12 +7,19 @@
 
 import Foundation
 
+enum EmptyTextField {
+    case emailEmpty
+    case passwordEmpty
+}
+
 protocol AuthViewModelInput {
-    func emailTextFieldEmptyVaild(input: String)
+    func textFieldEmptyVaild(email: String, password: String)
 }
 
 protocol AuthViewModelOutput {
-    var email: Observer<String?> { get set }
+    var email: String? {get set}
+    var password: String? {get set}
+    var textfildEmpty: Observer<EmptyTextField> {get set}
 }
 
 protocol AuthViewModelProtocol: AuthViewModelInput, AuthViewModelOutput {
@@ -22,18 +29,32 @@ final class AuthViewModel: AuthViewModelProtocol {
     
     //MARK: - output
     
-    var email: Observer<String?> = Observer(value: nil)
+    var email: String?
+    var password: String?
+    var textfildEmpty: Observer<EmptyTextField> = Observer(value: .emailEmpty)
 }
 
 extension AuthViewModel {
     
     //MARK: - input
     
-    func emailTextFieldEmptyVaild(input: String) {
-        if input == "" {
-            email.value = nil
+    func textFieldEmptyVaild(email: String, password: String) {
+        if email == "" {
+            textfildEmpty.value = .emailEmpty
+        } else if password == "" {
+            textfildEmpty.value = .passwordEmpty
         } else {
-            email.value = input
+            self.email = email
+            self.password = password
         }
     }
+    func textFieldEmptyString() -> String {
+        switch textfildEmpty.value {
+        case .emailEmpty:
+            return "아이디를 입력해주세요!"
+        case .passwordEmpty:
+            return "비빌번호를 입력해주세요!"
+        }
+    }
+    
 }
