e9d1a9ae1d31eff236f8a7514ff94d7b9adc13fc
DeokHo98 <ekdj98@gmail.com>
2022-05-30 15:24:47 +0900

    #92 refactor: 테스트코드 수정

--- Project-ClonePassOrder/ViewModel/Auth/Project-ClonePassOrderTests/AuthUnitTest.swift
+++ Project-ClonePassOrder/ViewModel/Auth/Project-ClonePassOrderTests/AuthUnitTest.swift
@@ -21,25 +21,49 @@ class AuthUnitTest: XCTestCase {
         try super.tearDownWithError()
         sut = nil
     }
-    func test_emailTextFieldEmptyVaild를_호출시_빈문자열을_전달했을때_email변수가_nil과_일치하는지() {
+    func test_textFieldEmptyVaild를_호출시_email_password_파라미터에_둘중_하나라도빈문자열을_전달했을때_email변수와_password변수가_모두_nil인지() {
         //given
-        let textFieldText = ""
+        let emailText = ""
+        let paswordText = "123123"
         
         //when
-        sut.emailTextFieldEmptyVaild(input: textFieldText)
+        sut.textFieldEmptyVaild(email: emailText, password: paswordText)
         
         //then
-        XCTAssertEqual(sut.email.value, nil, "이메일이 Nil이 아닙니다.")
+        XCTAssertNil(sut.email, "이메일이 Nil이 아닙니다.")
+        XCTAssertNil(sut.password, "패스워드가 Nil이 아닙니다")
     }
-    func test_emailTextFiledEmptyVaild를_호출시_비어있지않은_문자열을_전달했을때_email변수가_문자열과_일치하는지() {
+    func test_textFiledEmptyVaild를_호출시_email_password_파라미터에_둘모두_비어있지않은_문자열을_전달했을때_전달받은값과_변수가_모두일치하는지() {
         //given
-        let textFieldText = "qwer@gmail.com"
-        
+        let emailText = "qwer@naver.com"
+        let passwordText = "123456"
+
         //when
-        sut.emailTextFieldEmptyVaild(input: textFieldText)
+        sut.textFieldEmptyVaild(email: emailText, password: passwordText)
         
         //then
-        XCTAssertEqual(sut.email.value, textFieldText, "이메일과 textFieldText가 일치하지않습니다.")
+        XCTAssertEqual(sut.email, emailText, "이메일이 일치하지않습니다")
+        XCTAssertEqual(sut.password, passwordText, "패스워드가 일치하지않습니다")
+    }
+    func test_textFieldEmptyString을_호출시_textfieldEmptyValue값이_emailEmpty인경우_아이디를입력해주세요를_리턴하는지() {
+        //given
+        sut.textfildEmpty.value = .emailEmpty
+        
+        //when
+        let vaild = sut.textFieldEmptyString()
+        
+        //then
+        XCTAssertEqual(vaild,"아이디를 입력해주세요!", "리턴하지않습니다")
+    }
+    func test_textFieldEmptyString을_호출시_textfieldEmptyValue값이_passwordEmpty인경우_비밀번호를입력해주세요를_리턴하는지() {
+        //given
+        sut.textfildEmpty.value = .passwordEmpty
+        
+        //when
+        let vaild = sut.textFieldEmptyString()
+        
+        //then
+        XCTAssertEqual(vaild,"비빌번호를 입력해주세요!", "리턴하지않습니다")
     }
 
 }
