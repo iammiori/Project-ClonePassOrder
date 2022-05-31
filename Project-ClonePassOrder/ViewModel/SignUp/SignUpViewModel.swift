9149b0fe3d61f4f54d79b463a8635dcef0146500
DeokHo98 <ekdj98@gmail.com>
2022-05-31 12:27:20 +0900

    #91  회원가입 viewModel 생성

---
+++ Project-ClonePassOrder/ViewModel/SignUp/SignUpViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  SignUpViewModel.swift
+//  Project-ClonePassOrder
+//
+//  Created by 정덕호 on 2022/05/31.
+//
+
+import Foundation
+
+protocol SignUpViewModelInput {
+    
+}
+
+protocol SignUpViewModelOutput {
+    
+}
+
+protocol SignUpViewModelProtocol: SignUpViewModelInput, SignUpViewModelOutput {
+    
+}
+
+//MARK: - output
+
+final class SignUpViewModel: SignUpViewModelProtocol {
+    
+}
+
+//MARK: - input
+
+extension SignUpViewModel {
+    
+}
