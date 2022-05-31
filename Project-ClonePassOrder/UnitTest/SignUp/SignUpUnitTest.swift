1285a61c9499cbde5efb503aa7e54b5296c5d59f
DeokHo98 <ekdj98@gmail.com>
2022-05-31 12:27:30 +0900

    #92 회원가입 단위테스트 생성

---
+++ Project-ClonePassOrder/UnitTest/SignUp/SignUpUnitTest.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpUnitTest.swift
+//  UnitTest
+//
+//  Created by 정덕호 on 2022/05/31.
+//
+
+import XCTest
+
+class SignUpUnitTest: XCTestCase {
+    
+    let sut = SignUpViewModel
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    
+
+}
