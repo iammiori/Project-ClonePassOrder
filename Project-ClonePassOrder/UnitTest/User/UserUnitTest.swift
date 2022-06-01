8d7b66592f645bc7e36c9a6baafbcf30344f7a7a
DeokHo98 <ekdj98@gmail.com>
2022-06-01 14:23:50 +0900

    #100 userUnitTest

--- Project-ClonePassOrder/UnitTest/User/UserUnitTest.swift
+++ Project-ClonePassOrder/UnitTest/User/UserUnitTest.swift
@@ -6,17 +6,73 @@
 //
 
 import XCTest
+@testable import Project_ClonePassOrder
 
 class UserUnitTest: XCTestCase {
     
-    v
+    var sut: UserViewModel!
 
     override func setUpWithError() throws {
-        super.setUpWithError()
+       try super.setUpWithError()
+        sut = UserViewModel()
     }
 
     override func tearDownWithError() throws {
-        super.tearDownWithError()
+       try super.tearDownWithError()
+        sut = nil
+    }
+
+    func test_UserViewModel() {
+        //given
+        let model = UserModel(userName: "정한별",
+                              email: "aoao1216@naver.com",
+                              profileImageUrl: "profileimageUrl")
+        sut.model = model
+        
+        
+        //then
+        XCTAssertEqual(sut.userName, "정한별")
+        XCTAssertEqual(sut.profileImageUrl, "profileimageUrl")
+    }
+    func test_userFetch를_호출했을때_성공하는경우_model전달되는지() {
+        let uid = "fdg2qkdsalk234"
+        let model = UserModel(userName: "정한별",
+                              email: "",
+                              profileImageUrl: "profileimageUrl")
+        var mockUserService = MockUserService()
+        mockUserService.result = .success(model)
+        sut.userService = mockUserService
+        
+    
+        
+        sut.userFetch(uid: uid)
+        
+        XCTAssertEqual(sut.model.userName, model.userName)
+        XCTAssertEqual(sut.model.profileImageUrl, model.profileImageUrl)
+    }
+    func test_userFetch를_호출했을때_Firestore통신에실패하는경우_userFetchError를전달하는지() {
+        let uid = "fdg2qkdsalk234"
+        var mockUserService = MockUserService()
+        mockUserService.result = .failure(.userFetchError)
+        sut.userService = mockUserService
+        
+    
+        
+        sut.userFetch(uid: uid)
+        
+        XCTAssertEqual(sut.userServiceError.value, .userFetchError )
+    }
+    func test_userFetch를_호출했을때_snapshot데이터가_없는경우_snapShotError를전달하는지() {
+        let uid = "fdg2qkdsalk234"
+        var mockUserService = MockUserService()
+        mockUserService.result = .failure(.snapShotError)
+        sut.userService = mockUserService
+        
+    
+        
+        sut.userFetch(uid: uid)
+        
+        XCTAssertEqual(sut.userServiceError.value, .snapShotError)
     }
 
 
