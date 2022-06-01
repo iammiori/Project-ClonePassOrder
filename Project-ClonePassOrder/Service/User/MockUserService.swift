dfdee6a075fd226947d6dfd1635dc5a7cf4809d0
DeokHo98 <ekdj98@gmail.com>
2022-06-01 14:24:06 +0900

    #100 feat: userService

---
+++ Project-ClonePassOrder/Service/User/MockUserService.swift
@@ -0,0 +1,19 @@
+//
+//  MockUserService.swift
+//  Project-ClonePassOrder
+//
+//  Created by 정덕호 on 2022/06/01.
+//
+
+import Foundation
+
+struct MockUserService: UserServiceProtocol {
+    
+    var result: Result<UserModel, UserServiceError>?
+    
+    func fetch(uid: String, completion: @escaping (Result<UserModel, UserServiceError>) -> Void) {
+        completion(result!)
+    }
+    
+    
+}
