6a7565021e30b1d5cf1c297c39d4b55627f0c7b3
DeokHo98 <ekdj98@gmail.com>
2022-06-01 12:37:19 +0900

    #100 userUnitTest 생성

---
+++ Project-ClonePassOrder/UnitTest/User/UserUnitTest.swift
@@ -0,0 +1,23 @@
+//
+//  UserUnitTest.swift
+//  UnitTest
+//
+//  Created by 정덕호 on 2022/06/01.
+//
+
+import XCTest
+
+class UserUnitTest: XCTestCase {
+    
+    v
+
+    override func setUpWithError() throws {
+        super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        super.tearDownWithError()
+    }
+
+
+}
