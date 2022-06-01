a7cf9363d53a07be21cebaca87ca39d1c7d7c8ab
DeokHo98 <ekdj98@gmail.com>
2022-06-01 23:03:05 +0900

    #101 style: 코딩컨벤션 수정

--- Project-ClonePassOrder/UnitTest/User/UserUnitTest.swift
+++ Project-ClonePassOrder/UnitTest/User/UserUnitTest.swift
@@ -29,12 +29,12 @@ class UserUnitTest: XCTestCase {
                               profileImageUrl: "profileimageUrl")
         sut.model.value = model
         
-        
         //then
         XCTAssertEqual(sut.userName, "정한별")
         XCTAssertEqual(sut.profileImageUrl, URL(string: "profileimageUrl"))
     }
     func test_userFetch를_호출했을때_성공하는경우_model전달되는지() {
+        //given
         let uid = "fdg2qkdsalk234"
         let model = UserModel(userName: "정한별",
                               email: "",
@@ -43,35 +43,37 @@ class UserUnitTest: XCTestCase {
         mockUserService.result = .success(model)
         sut.userService = mockUserService
         
-    
-        
+        //when
         sut.userFetch(uid: uid)
         
+        //then
         XCTAssertEqual(sut.model.value.userName, model.userName)
         XCTAssertEqual(sut.model.value.profileImageUrl, model.profileImageUrl)
     }
     func test_userFetch를_호출했을때_Firestore통신에실패하는경우_userFetchError를전달하는지() {
+        //given
         let uid = "fdg2qkdsalk234"
         var mockUserService = MockUserService()
         mockUserService.result = .failure(.userFetchError)
         sut.userService = mockUserService
         
-    
-        
+        //when
         sut.userFetch(uid: uid)
         
+        //then
         XCTAssertEqual(sut.userServiceError.value, .userFetchError )
     }
     func test_userFetch를_호출했을때_snapshot데이터가_없는경우_snapShotError를전달하는지() {
+        //given
         let uid = "fdg2qkdsalk234"
         var mockUserService = MockUserService()
         mockUserService.result = .failure(.snapShotError)
         sut.userService = mockUserService
         
-    
-        
+        //when
         sut.userFetch(uid: uid)
         
+        //then
         XCTAssertEqual(sut.userServiceError.value, .snapShotError)
     }
 
