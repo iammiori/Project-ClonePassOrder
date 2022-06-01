893e030adb84024aae526865c4bfb9b7861c7d84
DeokHo98 <ekdj98@gmail.com>
2022-06-01 14:22:36 +0900

    #100 feat: userViewModel

---
+++ Project-ClonePassOrder/ViewModel/User/UserViewModel.swift
@@ -0,0 +1,63 @@
+//
+//  UserViewModel.swift
+//  Project-ClonePassOrder
+//
+//  Created by 정덕호 on 2022/06/01.
+//
+
+import Foundation
+
+protocol UserViewModelInput {
+    func userFetch(uid: String)
+}
+
+protocol UserViewModelOutput {
+    var model: Observer<UserModel> {get set}
+    var userName: String {get}
+    var profileImageUrl: String {get}
+    var userServiceError: Observer<UserServiceError> {get set}
+}
+
+protocol UserViewModelProtocol: UserViewModelInput, UserViewModelOutput {
+    
+}
+
+
+final class UserViewModel: UserViewModelProtocol {
+    
+    static let shared = UserViewModel()
+    
+    var userService: UserServiceProtocol
+    
+    //MARK: - outPut
+    
+    var model: Observer<UserModel> = Observer(value: UserModel.EMPTY)
+    
+    var userName: String {
+        return model.value.userName
+    }
+    var profileImageUrl: String {
+        return model.value.profileImageUrl
+    }
+    var userServiceError: Observer<UserServiceError> = Observer(value: .snapShotError)
+    
+    init(service: UserServiceProtocol = UserService()) {
+        self.userService = service
+    }
+}
+
+
+extension UserViewModel {
+    
+    //MARK: - input
+    func userFetch(uid: String) {
+        userService.fetch(uid: uid) { [weak self] result in
+            switch result {
+            case .success(let model):
+                self!.model.value = model
+            case .failure(let error):
+                self!.userServiceError.value = error
+            }
+        }
+    }
+}
