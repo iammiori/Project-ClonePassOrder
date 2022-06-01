83d37d1e2cc100ae5617cb58d57fb210a0573950
DeokHo98 <ekdj98@gmail.com>
2022-06-02 00:09:04 +0900

    #102 feat: 광고기능관련파일추가

---
+++ Project-ClonePassOrder/Model/AD/ADModel.swift
@@ -0,0 +1,8 @@
+//
+//  ADModel.swift
+//  Project-ClonePassOrder
+//
+//  Created by 정덕호 on 2022/06/02.
+//
+
+import Foundation
---
+++ Project-ClonePassOrder/Service/AD/MockADService.swift
@@ -0,0 +1,8 @@
+//
+//  MockADService.swift
+//  Project-ClonePassOrder
+//
+//  Created by 정덕호 on 2022/06/02.
+//
+
+import Foundation
