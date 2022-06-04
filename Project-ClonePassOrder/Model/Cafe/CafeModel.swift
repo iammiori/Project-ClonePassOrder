7448a2dc5e17926066dea4bed0c9dc5da33ce517
DeokHo98 <ekdj98@gmail.com>
2022-06-04 11:44:29 +0900

    #105 feat: cafe모델 구현

--- Project-ClonePassOrder/Model/Cafe/CafeModel.swift
+++ Project-ClonePassOrder/Model/Cafe/CafeModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct CafeModel: Equatable {
+    var name: String
+    var address: String
+    var storyCount: Int
+    var favoriteCount: Int
+    var imageURL: String
+    var info: String
+    var lat: Double
+    var lon: Double
+    var offDay: String
+    var openTime: String
+    var orderTime: String
+    var phoneNumber: String
+    var benefit: String
+    var newTime: String
+}
+
